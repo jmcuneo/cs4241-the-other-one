5d5bc781a6373845e05a7f766e1127590fd76497
Perez0002 <op90210@gmail.com>
2024-04-11 13:04:17 -0400

    added explanation page

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const server = http.createServer( function( request,response ) {
     case '/styles.css':
     sendFile( response, 'styles.css' )
       break
+    case '/explanation.html':
+      sendFile( response, 'explanation.html' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
