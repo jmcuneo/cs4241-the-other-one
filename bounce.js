73e0b7629d9a076a3f3b67c718203c452e2b5dab
AGChad <agchadbourne@wpi.edu>
2024-04-11 12:59:20 -0400

    basic movement acheived

--- bounce.js
+++ bounce.js
@@ -2,9 +2,35 @@ window.onload = () => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
-    ctx.beginPath();
-    ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.closePath();    
+    let x = canvas.width / 2;
+    let y = canvas.height / 2;
+    let dx = 2;
+    let dy = -2;
+
+
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        disk()
+        x += dx;
+        y += dy;
+    }
+
+    function disk(){
+        //Ivy's code replaces this:
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI * 2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    function startGame() {
+        const interval = setInterval(draw, 10);
+    }
+
+    startGame()
+
+
+         
 }
