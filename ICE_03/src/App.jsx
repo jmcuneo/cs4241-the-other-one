42a65a5bc808fc4418554ca13df6b12af3263217
Ronak Wani <ronak_wani@outlook.com>
2024-04-11 13:19:22 -0400

    HTML React

--- ICE_03/src/App.jsx
+++ ICE_03/src/App.jsx
@@ -4,32 +4,48 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1 class="outfit-header-font">Ticket Reservation System</h1>
+            <form>
+                <label for="FirstName">First Name: </label>
+                <input type="text" id="FirstName" placeholder="First Name" required="required"/>
+                <label for="MiddleName">Middle Name: </label>
+                <input type="text" id="MiddleName" placeholder="Middle Name"/>
+                <label for="LastName">Last Name: </label>
+                <input type="text" id="LastName" placeholder="Last Name"/> <br/>
+                <label for="Email">Email: </label>
+                <input type="email" id="Email" placeholder="Email" required="required"/>
+                <label for="StartLocation">Start Location: </label>
+                <input type="text" id="StartLocation" placeholder="USA Start Location" required="required"/>
+                <label for="Destination">Destination: </label>
+                <input type="text" id="Destination" placeholder="USA destination" required="required"/>
+            </form>
+            <label for="transport-mode" >Choose your transportation mode:</label>
+            <select name="Transportation mode" id="transport-mode">
+                <option value="Choose">Choose</option>
+                <option value="Lyft">Lyft</option>
+                <option value="Uber">Uber</option>
+                <option value="PeterPan Bus">PeterPan Bus</option>
+                <option value="Greyhound Bus">Greyhound Bus</option>
+                <option value="Our Bus">Our Bus</option>
+                <option value="Subway">Subway</option>
+                <option value="Commuter Rail">Commuter Rail</option>
+            </select><br/>
+            <button class="outfit-inside-buttons-font submit" type="Submit" id="Submit">Submit</button>
+            <button onclick="NewData()" type="button" id="Confirm">Confirm</button>
+            <div id="booking_details">
+                <table> <tr> <th> Unique ID </th><th> First Name </th> <th> Middle Name </th>
+                    <th> Last Name</th><th> Email </th> <th>Start Location</th> <th> Destination </th>
+                    <th>Mode of Transportation</th> <th>Cost</th> </tr><tbody id="data_body"></tbody>
+                </table>
+            </div>
+            <footer>
+                <p>Copyright &copy; Ronak Wani. All rights reserved.</p>
+            </footer>
+        </>
+    )
 }
-
 export default App
