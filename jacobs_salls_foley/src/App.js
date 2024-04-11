5ccecee0a3ab7bb63978f464e2eae81ee6928cf3
lfoley7 <luke.c.foley@gmail.com>
2024-04-11 13:27:36 -0400

    second push

--- jacobs_salls_foley/src/App.js
+++ jacobs_salls_foley/src/App.js
@@ -1,42 +1,41 @@
 import logo from './logo.svg';
-import './javascript/Main.js';
+import Main from './javascript/Main';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <body>
-        <h1>Enter a String to Anagramize!</h1>
-        <form id="mainform">
-          <input type="text" id="string" value="enter string here!" />
-          <button id="submit">Generate!</button>
-        </form>
-        <div id="table">
-          <span>
-            <b>String</b>
-          </span>
-          <span>
-            <b>Anagram 1</b>
-          </span>
-          <span>
-            <b>Anagram 2</b>
-          </span>
-          <span>
-            <b>Anagram 3</b>
-          </span>
-          <span>
-            <b>Anagram 4</b>
-          </span>
-          <span>
-            <b>Remove</b>
-          </span>
-        </div>
-        <footer>
-          <p>
-            Trash can icons created by <a href="https://www.flaticon.com/free-icons/trash-can" title="trash can icons">kliwir art - Flaticon</a>
-          </p>
-        </footer>
-      </body>
+      <h1>Enter a String to Anagramize!</h1>
+      <form id="mainform">
+        <input type="text" id="string" placeholder="enter string here!" />
+        <Main />
+        <button id="submit">Generate!</button>
+      </form>
+      <div id="table">
+        <span>
+          <b>String</b>
+        </span>
+        <span>
+          <b>Anagram 1</b>
+        </span>
+        <span>
+          <b>Anagram 2</b>
+        </span>
+        <span>
+          <b>Anagram 3</b>
+        </span>
+        <span>
+          <b>Anagram 4</b>
+        </span>
+        <span>
+          <b>Remove</b>
+        </span>
+      </div>
+      <footer>
+        <p>
+          Trash can icons created by <a href="https://www.flaticon.com/free-icons/trash-can" title="trash can icons">kliwir art - Flaticon</a>
+        </p>
+      </footer>
     </div>
   );
 }
