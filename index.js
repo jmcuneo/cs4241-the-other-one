d207cac94588aa446a9a68671a9ab0494c07bf44
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-04-11 13:32:58 -0400

    Implemented game

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ explanationButton.addEventListener('click', async () => {
 });
 
 startButton.addEventListener('click', async () => {
-
+    init_grid();
 });
 
 nextButton.addEventListener('click', async () => {
-
+    update(grid);
 });
 
 resetButton.addEventListener('click', async () => {
@@ -25,13 +25,13 @@ const ctx = canvas.getContext('2d');
 canvas.width = 512;
 canvas.height = 512;
 
-let items = [[0, 1, 1, 0], [1, 0, 0, 1], [0, 0, 0, 0], [1, 0, 1, 1]];
 function draw() {
     window.requestAnimationFrame(draw);
-
-    for (let i = 0; i < items.length; i++) {
-        for (let j = 0; j < items[i].length; j++) {
-            if (items[i][j] === 1) {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) {
+                ctx.fillRect(i * 10, j * 10, Math.floor(canvas.width / 10), Math.floor(canvas.height / 10));
+            } else {
                 ctx.strokeRect(i * 10, j * 10, Math.floor(canvas.width / 10), Math.floor(canvas.height / 10));
             }
         }
