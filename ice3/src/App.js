a8559864bea5aa2a515ba8fa96e99d981f06394c
Aidan MacNevin <ahmacnevin@wpi.edu>
2024-04-11 13:16:25 -0400

    General loading page

--- ice3/src/App.js
+++ ice3/src/App.js
@@ -2,23 +2,86 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  async function table() {
+    const response = await fetch("/appdata", {
+      method: "GET"
+    })
+    const text = await response.text()
+    const data = JSON.parse(text);
+
+    return (
+        <div className="App">
+          <table>
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Birthday</th>
+              <th>Cake</th>
+              <th>More</th>
+            </tr>
+            {data.map((val, key) => {
+              return (
+                  <tr key={key}>
+                    <td>{val.id}</td>
+                    <td>{val.name}</td>
+                    <td>{val.age}</td>
+                    <td>{val.birthday}</td>
+                    <td>{val.preferredCake}</td>
+                  </tr>
+              )
+            })}
+          </table>
+        </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Data Table</h1>
+    <table id="datatable">
+    </table>
+
+
+    <div id="all-forms-container" className="flex-row">
+
+      <div id="form-new-container" className="form-container">
+        <h3 className="form-header">Form for New Entry</h3>
+        <form id="form-new" onKeyDown="if(event.keyCode === 13) {
+          return false;
+        }">
+          <label>Name:</label>
+          <input type="text" id="yourname" placeholder="your name here"/>
+            <label>Birthday (MM/DD/YYYY):</label>
+            <input type="text" id="yourbday" placeholder="your bday here"/>
+              <label>Preferred Cake Flavor:</label>
+              <input type="text" id="yourcake" placeholder="your preferred cake flavor here"/>
+                <button id="submit">submit</button>
+        </form>
+      </div>
+
+      <div id="form-update-container" className="form-container">
+        <h3 className="form-header">Form for Updating Entry (Click on an ID in table)</h3>
+        <form id="form-update" onKeyDown="if(event.keyCode === 13) {
+          return false;
+        }">
+          <label>ID:</label>
+          <input type="number" id="id" disabled/>
+            <label>Name:</label>
+            <input type="text" id="name" placeholder="name here"/>
+              <label>Age:</label>
+              <input type="number" id="age" disabled/>
+                <label>Birthday (MM/DD/YYYY):</label>
+                <input type="text" id="bday" placeholder="bday here"/>
+                  <label>Preferred Cake Flavor:</label>
+                  <input type="text" id="cake" placeholder="preferred cake flavor here"/>
+                    <button id="update">update</button>
+        </form>
+      </div>
+
     </div>
+  </div>
   );
 }
 
