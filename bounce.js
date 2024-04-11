39140102839af52bd37b188db58a1e670a21dca2
AGChad <agchadbourne@wpi.edu>
2024-04-11 13:05:00 -0400

    wall bounce added

--- bounce.js
+++ bounce.js
@@ -1,17 +1,25 @@
 window.onload = () => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-
+    const ballRadius = 10;
     let x = canvas.width / 2;
     let y = canvas.height / 2;
-    let dx = 2;
-    let dy = -2;
+    let dx = 4;
+    let dy = -1;
 
 
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         disk()
+
+        if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+            dx = -dx;
+          }
+        if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+            dy = -dy;
+        }
+
         x += dx;
         y += dy;
     }
