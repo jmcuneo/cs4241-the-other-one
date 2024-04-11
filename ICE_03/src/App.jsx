7e1d552628e0233e3614fed4e68054528713cda4
Ronak Wani <ronak_wani@outlook.com>
2024-04-11 13:27:20 -0400

    HTML changes

--- ICE_03/src/App.jsx
+++ ICE_03/src/App.jsx
@@ -8,22 +8,22 @@ function App() {
 
     return (
         <>
-            <h1 class="outfit-header-font">Ticket Reservation System</h1>
+            <h1 className="outfit-header-font">Ticket Reservation System</h1>
             <form>
-                <label for="FirstName">First Name: </label>
+                <label htmlFor="FirstName">First Name: </label>
                 <input type="text" id="FirstName" placeholder="First Name" required="required"/>
-                <label for="MiddleName">Middle Name: </label>
+                <label htmlFor="MiddleName">Middle Name: </label>
                 <input type="text" id="MiddleName" placeholder="Middle Name"/>
-                <label for="LastName">Last Name: </label>
+                <label htmlFor="LastName">Last Name: </label>
                 <input type="text" id="LastName" placeholder="Last Name"/> <br/>
-                <label for="Email">Email: </label>
+                <label htmlFor="Email">Email: </label>
                 <input type="email" id="Email" placeholder="Email" required="required"/>
-                <label for="StartLocation">Start Location: </label>
+                <label htmlFor="StartLocation">Start Location: </label>
                 <input type="text" id="StartLocation" placeholder="USA Start Location" required="required"/>
-                <label for="Destination">Destination: </label>
+                <label htmlFor="Destination">Destination: </label>
                 <input type="text" id="Destination" placeholder="USA destination" required="required"/>
             </form>
-            <label for="transport-mode" >Choose your transportation mode:</label>
+            <label htmlFor="transport-mode" >Choose your transportation mode:</label>
             <select name="Transportation mode" id="transport-mode">
                 <option value="Choose">Choose</option>
                 <option value="Lyft">Lyft</option>
@@ -34,8 +34,8 @@ function App() {
                 <option value="Subway">Subway</option>
                 <option value="Commuter Rail">Commuter Rail</option>
             </select><br/>
-            <button class="outfit-inside-buttons-font submit" type="Submit" id="Submit">Submit</button>
-            <button onclick="NewData()" type="button" id="Confirm">Confirm</button>
+            <button className="outfit-inside-buttons-font submit" type="Submit" id="Submit">Submit</button>
+            <button onClick="NewData()" type="button" id="Confirm">Confirm</button>
             <div id="booking_details">
                 <table> <tr> <th> Unique ID </th><th> First Name </th> <th> Middle Name </th>
                     <th> Last Name</th><th> Email </th> <th>Start Location</th> <th> Destination </th>
