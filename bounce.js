628a4e6214d2ca9e4d3e40b268b5a1928f6d5839
AGChad <agchadbourne@wpi.edu>
2024-04-11 12:50:20 -0400

    error fixed

--- bounce.js
+++ bounce.js
@@ -1,8 +1,10 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+window.onload = () => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();    
+}
